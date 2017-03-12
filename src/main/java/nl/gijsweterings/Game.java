8d97be179480bc6e265231ce9a8100ea5639e683
Gijs Weterings <gijsweterings+github@gmail.com>
2017-03-12 22:13:53 +0100

    Switch to StateBasedGame

--- src/main/java/nl/gijsweterings/Game.java
+++ src/main/java/nl/gijsweterings/Game.java
@@ -1,43 +1,48 @@
 package nl.gijsweterings;
 
+import nl.gijsweterings.game.states.GameOverState;
+import nl.gijsweterings.game.states.RunningState;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
 
 /**
  * A game using Slick2d
  */
-public class Game extends BasicGame {
+public class Game extends StateBasedGame {
 
     /** Screen width */
     private static final int WIDTH = 800;
     /** Screen height */
     private static final int HEIGHT = 600;
+    /** Desired framerate */
+    private static final int TARGET_FRAME_RATE = 60;
 
     public Game() {
         super("Agar.io clone");
     }
 
-    public void render(GameContainer container, Graphics g) throws SlickException {
-        g.drawString("Welcome to Agar.io!", 50, 50);
-    }
-
     @Override
-    public void init(GameContainer container) throws SlickException {
-        // TODO set up world, spawn entities
+    public void initStatesList(GameContainer gameContainer) throws SlickException {
+        addState(new RunningState());
+        addState(new GameOverState());
     }
 
-    @Override
-    public void update(GameContainer container, int delta) throws SlickException {
-        // TODO listen for keyboard input, apply movement vector
+    public static int getScreenWidth() {
+        return Game.WIDTH;
     }
 
-    
+    public static int getScreenHeight() {
+        return Game.HEIGHT;
+    }
+
     public static void main(String[] args) throws SlickException {
         AppGameContainer app = new AppGameContainer(new Game());
         app.setDisplayMode(WIDTH, HEIGHT, false);
+        app.setTargetFrameRate(TARGET_FRAME_RATE);
         app.setForceExit(false);
         app.start();
     }
